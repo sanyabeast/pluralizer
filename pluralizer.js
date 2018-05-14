53d833b553d75dc8dc41334b6a3e7065df600759
sanyabeast <a.gvrnsk@gmail.com>
2018-05-14 17:47:51 +0300

    Major string parsing

--- pluralizer.js
+++ pluralizer.js
@@ -18,11 +18,10 @@
     Pluralizer.prototype = {
         expressions : {
             range : function(input, from, to){
-                console.log(this);
                 var expName = ["range", from, to].join("@");
-                if (this.__cahced && this.__cahced[expName]){
+                /*if (this.__cahced && this.__cahced[expName]){
                     return this.__cahced[expName];
-                }
+                }*/
 
                 var regexp = "";
                 for (var a = from; a <= to; a++){
@@ -32,12 +31,20 @@
                 regexp = regexp.substring(0, regexp.length - 1);
                 regexp = ["(", regexp, ")"].join("");
 
-                this.__cahced[expName] = regexp;
+                /*this.__cahced[expName] = regexp;*/
 
                 return regexp;
             },
+            or : function(){
+                return Array.prototype.indexOf.call(arguments, true) > -1;
+            },
+            and : function(){
+                return Array.prototype.indexOf.call(arguments, false) < 0;
+            },
+            some : function(){
+                return ["(", Array.prototype.join.call(arguments, "|"), ")"].join("");
+            },
             ends : function(input, value){
-                console.log(value);
                 return input.toString().match(new RegExp([value, "$"].join(""))) != null;
             },
             equals : function(input, value){
@@ -63,8 +70,42 @@
         setRules : function(rulesString){
             this.rules = this.__parseRules(rulesString);
         },
-        make : function(){
+        make : function(inputString, vars){
+            return this.__parseString(inputString, vars);
+        },
+        __parseString : function(inputString, vars){
+            var result = inputString;
+            var tokens = inputString.match(/\#{[^${]*}/g);
+            
+            this.tools.loop(tokens, function(token, index){
+                var cleanToken = token.substring(2, token.length - 1);
+                var varName = cleanToken.split("?")[1];
+                var forms = cleanToken.split("?")[0].split("@");
 
+
+                if (forms.length && typeof vars[varName] !== "undefined"){
+                    result = result.replace(token, forms[this.__getMatchedRule(vars[varName])] || "");
+                } else if (forms.length){
+                    result = result.replace(token, forms[0]);
+                } else {
+                    result = result.replace(token, "");
+                }
+
+            }, this);
+
+            return result;
+
+        },
+        __getMatchedRule : function(value){
+            var result = 0;
+
+            this.tools.loop(this.rules, function(checker, index){
+                if (checker(value)){
+                    result = index;
+                }
+            });
+
+            return result;
         },
         __parseRules : function(rulesString){
             var splitted = rulesString.split("#");
@@ -75,6 +116,9 @@
                 token = token.replace(/[?(]/g, "(v,");
                 rules[a] = (function(
                     range,
+                    or,
+                    and,
+                    some,
                     ends,
                     equals,
                     any
@@ -83,6 +127,9 @@
                     return eval(ruleCheckerCode);
                 })(
                     e.range.bind(this),
+                    e.or.bind(this),
+                    e.and.bind(this),
+                    e.some.bind(this),
                     e.ends.bind(this),
                     e.equals.bind(this),
                     e.any.bind(this)
